24d3778bf2f6f4d69f5cdfef15f0b204485e2de3
avenger-cyber <75252017+avenger-cyber@users.noreply.github.com>
2021-01-29 16:54:36 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,20 @@
+class Bird extends Base{
+    constructor(x, y) {
+
+      super(x, y, 50,50);
+
+      this.image = loadImage("sprites/bird.png");
+      
+      World.add(world, this.body);
+    };
+    
+  
+
+  display(){
+    var pos = this.body.position;
+    pos.x = mouseX;
+    pos.y = mouseY;
+
+   super.display();
+  }
+}
